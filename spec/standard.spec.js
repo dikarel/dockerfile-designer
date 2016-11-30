a0d51fbd24a64938ed8d82f61d5e7b52e3482b08
Darius I. Karel <me@dikarel.com>
2016-12-01 00:07:03 +0700

    spec: longer timeout for standard

--- spec/standard.spec.js
+++ spec/standard.spec.js
@@ -1,5 +1,8 @@
 /* eslint-env mocha */
 
-describe('project', () => {
+describe('project', function () {
+  // Extra leeway for standard to parse the entire project
+  this.timeout(15000)
+
   it('conforms to StandardJS', require('mocha-standard'))
 })
